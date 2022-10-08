af30e16c6a656b91f6a9ec735954938b419507ed
Alex Coats <swifteralex@users.noreply.github.com>
2022-10-08 11:14:18 -0700

    made changes to problem.py file

--- problem.py
+++ problem.py
@@ -8,7 +8,7 @@ class Heuristic(Enum):
     MANHATTAN_DISTANCE = 2
 
 
-def heuristic_score(node):
+def __heuristic_score(node):
     score = 0
     if heuristic == Heuristic.NONE:
         return score
@@ -31,6 +31,61 @@ def heuristic_score(node):
         return score
 
 
+def __find_blank(node):
+    for i in range(len(node.state)):
+        for j in range(len(node.state)):
+            if node.state[i][j] == 0:
+                return i, j
+
+
+def __up_op(node):
+    blank_i, blank_j = __find_blank(node)
+    if blank_i == 0:
+        return None
+    new_node = copy.deepcopy(node)
+    new_node.depth += 1
+    new_node.state[blank_i - 1][blank_j] = 0
+    new_node.state[blank_i][blank_j] = node.state[blank_i - 1][blank_j]
+    new_node.cost = new_node.depth + __heuristic_score(new_node)
+    return new_node
+
+
+def __down_op(node):
+    blank_i, blank_j = __find_blank(node)
+    if blank_i + 1 >= len(node.state):
+        return None
+    new_node = copy.deepcopy(node)
+    new_node.depth += 1
+    new_node.state[blank_i + 1][blank_j] = 0
+    new_node.state[blank_i][blank_j] = node.state[blank_i + 1][blank_j]
+    new_node.cost = new_node.depth + __heuristic_score(new_node)
+    return new_node
+
+
+def __left_op(node):
+    blank_i, blank_j = __find_blank(node)
+    if blank_j == 0:
+        return None
+    new_node = copy.deepcopy(node)
+    new_node.depth += 1
+    new_node.state[blank_i][blank_j - 1] = 0
+    new_node.state[blank_i][blank_j] = node.state[blank_i][blank_j - 1]
+    new_node.cost = new_node.depth + __heuristic_score(new_node)
+    return new_node
+
+
+def __right_op(node):
+    blank_i, blank_j = __find_blank(node)
+    if blank_j >= len(node.state):
+        return None
+    new_node = copy.deepcopy(node)
+    new_node.depth += 1
+    new_node.state[blank_i][blank_j + 1] = 0
+    new_node.state[blank_i][blank_j] = node.state[blank_i][blank_j + 1]
+    new_node.cost = new_node.depth + __heuristic_score(new_node)
+    return new_node
+
+
 def goal_test(state):
     for i in range(len(state)):
         for j in range(len(state)):
@@ -41,64 +96,6 @@ def goal_test(state):
     return True
 
 
-def __find_blank(node):
-    for i in range(len(node.state)):
-        for j in range(len(node.state)):
-            if node.state[i][j] == 0:
-                return i, j
-
-
-def __up_op(node):
-    if node.blank_pos[0] == 0:
-        return None
-    blank_i, blank_j = __find_blank
-    new_node = copy.deepcopy(node)
-    new_node.depth += 1
-    new_node.state[blank_i - 1][blank_j] = 0
-    new_node.state[blank_i][blank_j] = node.state[blank_i - 1][blank_j]
-    new_node.blank_pos[0] -= 1
-    new_node.cost = new_node.depth + heuristic_score(new_node, heuristic)
-    return new_node
-
-
-def __down_op(node):
-    if node.blank_pos[0] + 1 >= len(node.state):
-        return None
-    blank_i, blank_j = __find_blank
-    new_node = copy.deepcopy(node)
-    new_node.depth += 1
-    new_node.state[blank_i + 1][blank_j] = 0
-    new_node.state[blank_i][blank_j] = node.state[blank_i + 1][blank_j]
-    new_node.blank_pos[0] += 1
-    new_node.cost = new_node.depth + heuristic_score(new_node, heuristic)
-    return new_node
-
-
-def __left_op(node):
-    if node.blank_pos[1] == 0:
-        return None
-    blank_i, blank_j = __find_blank
-    new_node = copy.deepcopy(node)
-    new_node.depth += 1
-    new_node.state[blank_i][blank_j - 1] = 0
-    new_node.state[blank_i][blank_j] = node.state[blank_i][blank_j - 1]
-    new_node.blank_pos[1] -= 1
-    new_node.cost = new_node.depth + heuristic_score(new_node, heuristic)
-    return new_node
-
-
-def __right_op(node):
-    if node.blank_pos[1] + 1 >= len(node.state):
-        return None
-    blank_i, blank_j = __find_blank
-    new_node = copy.deepcopy(node)
-    new_node.depth += 1
-    new_node.state[blank_i][blank_j + 1] = 0
-    new_node.state[blank_i][blank_j] = node.state[blank_i][blank_j + 1]
-    new_node.blank_pos[1] += 1
-    new_node.cost = new_node.depth + heuristic_score(new_node, heuristic)
-    return new_node
-
-
+initial_state = []
 heuristic = Heuristic.NONE
 operators = [__up_op, __down_op, __left_op, __right_op]
