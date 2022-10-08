c0b9838e411c0dd9bc479c0a9cf1bacfe6ce1c75
Alex Coats <swifteralex@users.noreply.github.com>
2022-10-07 20:36:06 -0700

    refactored state to be a 2d array instead of a string; refactored user input

--- cs170_project1.py
+++ cs170_project1.py
@@ -5,42 +5,55 @@ from dataclasses import dataclass, field
 @dataclass(order=True)
 class PrioritizedItem:
     cost: int
-    state: str = field(compare=False)
+    state: list = field(compare=False)
+    blank_pos: list = field(compare=False)
+
+
+def expand(node):
+    list = []
+
+    return list
 
 
 def main():
-    puzzle_size = 0  # Length of each side of the puzzle
-    found_states = {}  # Dictionary of seen states; used to avoid loops
-    initial_state = ""  # Initial state; stored as a string of integers
-    text = input("Please provide the initial state as a non-separated list of integers."
-                 "Enter 0 for a blank. Press enter with each new row.\n")
-    puzzle_size = len(text)
-    for i in range(0, puzzle_size - 1):
-        initial_state += input()
-    print(initial_state)
-    print(puzzle_size)
+    initial_node = PrioritizedItem(0, [], [])
+
+    # Fill in initial state with user input
+    print("Please provide the initial state as a space-separated list of integers. Enter 0 for a blank. Press enter with each new row.")
+    while len(initial_node.state) == 0 or len(initial_node.state) < len(initial_node.state[0]):
+        initial_node.state.append([int(number) for number in input().split()])
+
+    # Find position of blank space
+    for i in range(len(initial_node.state)):
+        for j in range(len(initial_node.state)):
+            if initial_node.state[i][j] == 0:
+                initial_node.blank_pos = [i, j]
+
+    print(initial_node.state)
+    print(initial_node.blank_pos)
+    print(expand(initial_node))
 
 
 if __name__ == "__main__":
     main()
 
-item1 = PrioritizedItem(10, "1 2 4 5")
-item2 = PrioritizedItem(15, " test")
-item3 = PrioritizedItem(5, "b 1 2 3 4")
-item4 = PrioritizedItem(5, "1 4")
-h = []
-heapq.heappush(h, item1)
-heapq.heappush(h, item2)
-heapq.heappush(h, item3)
-heapq.heappush(h, item4)
-print(heapq.heappop(h))
-print(heapq.heappop(h))
-print(heapq.heappop(h))
-print(heapq.heappop(h))
-
-found_states = {}
-found_states[item1.state] = True
-found_states[item2.state] = True
-
-print(found_states.get(item3.state))
-print(found_states.get(item2.state))
+# item1 = PrioritizedItem(10, "1 2 4 5")
+# item2 = PrioritizedItem(15, " test")
+# item3 = PrioritizedItem(5, "b 1 2 3 4")
+# item4 = PrioritizedItem(5, "1 4")
+# h = []
+# heapq.heappush(h, item1)
+# heapq.heappush(h, item2)
+# heapq.heappush(h, item3)
+# heapq.heappush(h, item4)
+# print(heapq.heappop(h))
+# print(heapq.heappop(h))
+# print(heapq.heappop(h))
+# print(heapq.heappop(h))
+#
+# found_states = {}
+# found_states[item1.state] = True
+# found_states[item2.state] = True
+#
+# print(found_states.get(item3.state))
+# print(found_states.get(item2.state))
