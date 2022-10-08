cc658937bdd0237fec541e27a1800adc2a552e39
Alex Coats <swifteralex@users.noreply.github.com>
2022-10-07 22:35:08 -0700

    added heuristic functions

--- cs170_project1.py
+++ cs170_project1.py
@@ -1,13 +1,21 @@
 import heapq
 from dataclasses import dataclass, field
+from enum import Enum
 import copy
 
 
 @dataclass(order=True)
-class PrioritizedItem:
+class Node:
     cost: int
+    depth: int = field(compare=False)
     state: list = field(compare=False)
     blank_pos: list = field(compare=False)
+
+
+class Heuristic(Enum):
+    NONE = 0
+    MISPLACED_TILE = 1
+    MANHATTAN_DISTANCE = 2
 
 
 def expand(node):
@@ -16,38 +24,67 @@ def expand(node):
     blank_j = node.blank_pos[1]
     if node.blank_pos[0] > 0:  # Check if moving blank up is valid
         new_node = copy.deepcopy(node)
-        new_node.cost += 1
+        new_node.depth += 1
         new_node.state[blank_i - 1][blank_j] = 0
         new_node.state[blank_i][blank_j] = node.state[blank_i - 1][blank_j]
         new_node.blank_pos[0] -= 1
+        new_node.cost = new_node.depth + heuristic_score(new_node)
         new_node_list.append(new_node)
     if node.blank_pos[0] + 1 < len(node.state):  # Check if moving blank down is valid
         new_node = copy.deepcopy(node)
-        new_node.cost += 1
+        new_node.depth += 1
         new_node.state[blank_i + 1][blank_j] = 0
         new_node.state[blank_i][blank_j] = node.state[blank_i + 1][blank_j]
         new_node.blank_pos[0] += 1
+        new_node.cost = new_node.depth + heuristic_score(new_node)
         new_node_list.append(new_node)
     if node.blank_pos[1] > 0:  # Check if moving blank left is valid
         new_node = copy.deepcopy(node)
-        new_node.cost += 1
+        new_node.depth += 1
         new_node.state[blank_i][blank_j - 1] = 0
         new_node.state[blank_i][blank_j] = node.state[blank_i][blank_j - 1]
         new_node.blank_pos[1] -= 1
+        new_node.cost = new_node.depth + heuristic_score(new_node)
         new_node_list.append(new_node)
     if node.blank_pos[1] + 1 < len(node.state):  # Check if moving blank right is valid
         new_node = copy.deepcopy(node)
-        new_node.cost += 1
+        new_node.depth += 1
         new_node.state[blank_i][blank_j + 1] = 0
         new_node.state[blank_i][blank_j] = node.state[blank_i][blank_j + 1]
         new_node.blank_pos[1] += 1
+        new_node.cost = new_node.depth + heuristic_score(new_node)
         new_node_list.append(new_node)
     return new_node_list
 
 
-def main():
-    initial_node = PrioritizedItem(0, [], [])
+def heuristic_score(node):
+    score = 0
+    if heuristic == Heuristic.NONE:
+        return score
+    elif heuristic == Heuristic.MISPLACED_TILE:
+        for i in range(len(node.state)):
+            for j in range(len(node.state)):
+                if node.state[i][j] == 0:
+                    continue
+                if node.state[i][j] != i * len(node.state) + j + 1:
+                    score += 1
+        return score
+    elif heuristic == Heuristic.MANHATTAN_DISTANCE:
+        for i in range(len(node.state)):
+            for j in range(len(node.state)):
+                if node.state[i][j] == 0:
+                    continue
+                goal_i = (node.state[i][j] - 1) // len(node.state)
+                goal_j = (node.state[i][j] - 1) % len(node.state)
+                score += abs(goal_i - i) + abs(goal_j - j)
+        return score
 
+
+heuristic = Heuristic.MANHATTAN_DISTANCE
+initial_node = Node(0, 0, [], [])
+
+
+def main():
     # Fill in initial state with user input
     print("Please provide the initial state as a space-separated list of integers. Enter 0 for a blank. Press enter with each new row.")
     while len(initial_node.state) == 0 or len(initial_node.state) < len(initial_node.state[0]):
@@ -59,8 +96,6 @@ def main():
             if initial_node.state[i][j] == 0:
                 initial_node.blank_pos = [i, j]
 
-    print(initial_node.state)
-    print(initial_node.blank_pos)
     print(expand(initial_node))
 
 
