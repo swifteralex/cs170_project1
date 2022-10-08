c20b0d7fcb82a64dee87ca974ba65b66d2c1a5f7
Alex Coats <swifteralex@users.noreply.github.com>
2022-10-08 11:18:28 -0700

    added comments to problem.py

--- problem.py
+++ problem.py
@@ -2,42 +2,47 @@ import copy
 from enum import Enum
 
 
+# ######## Heuristic objects ######## #
 class Heuristic(Enum):
     NONE = 0
     MISPLACED_TILE = 1
     MANHATTAN_DISTANCE = 2
 
 
-def __heuristic_score(node):
+def __heuristic_score(state):
     score = 0
     if heuristic == Heuristic.NONE:
         return score
     elif heuristic == Heuristic.MISPLACED_TILE:
-        for i in range(len(node.state)):
-            for j in range(len(node.state)):
-                if node.state[i][j] == 0:
+        for i in range(len(state)):
+            for j in range(len(state)):
+                if state[i][j] == 0:
                     continue
-                if node.state[i][j] != i * len(node.state) + j + 1:
+                if state[i][j] != i * len(state) + j + 1:
                     score += 1
         return score
     elif heuristic == Heuristic.MANHATTAN_DISTANCE:
-        for i in range(len(node.state)):
-            for j in range(len(node.state)):
-                if node.state[i][j] == 0:
+        for i in range(len(state)):
+            for j in range(len(state)):
+                if state[i][j] == 0:
                     continue
-                goal_i = (node.state[i][j] - 1) // len(node.state)
-                goal_j = (node.state[i][j] - 1) % len(node.state)
+                goal_i = (state[i][j] - 1) // len(state)
+                goal_j = (state[i][j] - 1) % len(state)
                 score += abs(goal_i - i) + abs(goal_j - j)
         return score
+# ################################### #
 
 
+# ######### Helper functions ######## #
 def __find_blank(node):
     for i in range(len(node.state)):
         for j in range(len(node.state)):
             if node.state[i][j] == 0:
                 return i, j
+# ################################### #
 
 
+# ############ Operators ############ #
 def __up_op(node):
     blank_i, blank_j = __find_blank(node)
     if blank_i == 0:
@@ -84,6 +89,7 @@ def __right_op(node):
     new_node.state[blank_i][blank_j] = node.state[blank_i][blank_j + 1]
     new_node.cost = new_node.depth + __heuristic_score(new_node)
     return new_node
+# ################################### #
 
 
 def goal_test(state):
